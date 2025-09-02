8dfe032812536baea7150b5577ca9d9cf89b3c64
Maria Gabriela von Adamek <gabiadamek30@gmail.com>
2025-09-02 20:53:43 -0300

    tentativa dois

--- exercicio1.m
+++ exercicio1.m
@@ -9,9 +9,9 @@ imax = 20;
 
 t_roots = zeros(imax,1);
 t_roots(1)= x0;
-erro = zeros(lenght(t_roots),1);
+erro = zeros(length(t_roots),1);
 
-for ii = 1:lenght(t_roots)-1
+for ii = 1:length(t_roots)-1
     if ii ~= 1
       erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii));
         if erro(ii)<es
